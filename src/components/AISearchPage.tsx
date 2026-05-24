7e17dda038f39b48181e437176723e42d06726c5
lzzzhh <634885793@qq.com>
2026-05-24 23:57:32 +1000

    fix: missing )} after fragment close in ternary

--- src/components/AISearchPage.tsx
+++ src/components/AISearchPage.tsx
@@ -321,7 +321,7 @@ export default function AISearchPage({ onBack, onEnterStudy }: Props) {
               ))}
             </div>
           )}
-          </>
+          </>)}
         </div>
       </div>
     </div>
